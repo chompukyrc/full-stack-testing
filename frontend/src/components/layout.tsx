bd05a4ac3716af7948ceadbb861d09e167befc55
Kanyarat Chaiyanboon <kanyaratchaiyanboon527@gmail.com>
2025-02-22 18:18:30 +0700

    [frontend] - initial side bar

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import Header from "./header";
+// import Header from "./header";
+import Sidebar from "./sideBar";
 
 import { ReactNode } from "react";
 
 const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
     return (
-        <>
-            <Header />
+        <div className="flex">
+            <Sidebar />
             {children}
-        </>
+        </div>
     );
 };
 
